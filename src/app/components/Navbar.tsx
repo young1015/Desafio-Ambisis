62bf523ff86484a97c6445d0c73afb90a5c07036
young1015 <young1015@gmail.com>
2023-10-20 14:46:15 -0300

    Página principal adicionada

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import React from "react"
+import { BsFillBuildingFill } from 'react-icons/bs';
+
+const Navbar = () => {
+    return (
+        <nav className="flex space-x-5 border-b mb-5 px-5 items-center h-14">
+            <Link href="/"><BsFillBuildingFill/></Link>
+            <ul className="flex space-x-5 ">
+                <li><Link className="text-zinc-500 hover:text-slate-800 transition-colors text-lg" href="empresas">Cadastrar Empresa</Link></li>
+                <li><Link className="text-zinc-500 hover:text-slate-800 transition-colors text-lg" href="licenca">Licenças</Link></li>
+            </ul>   
+        </nav>
+    )
+
+}
+
+export default Navbar
